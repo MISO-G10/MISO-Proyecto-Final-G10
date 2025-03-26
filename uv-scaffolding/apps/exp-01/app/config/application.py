1f05dff77a5619eeee374ee3603f67f2322abf42
Andres Garcia <oa.garcia2@uniandes.edu.co>
2025-03-26 13:07:08 -0500

    modificación estructura de experimentos

--- uv-scaffolding/apps/exp-01/app/config/application.py
+++
@@ -1,8 +0,0 @@
-import os
-
-
-class ApplicationConfig:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
-    JWT_VERIFY_SUB = True
