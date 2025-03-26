7abcd17a5d8b18fc6bf694858fe87472c5379fb8
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-03-25 23:54:58 -0500

    actualizacion angular 19

--- WebApp/src/app/features/private/private.component.ts
+++ WebApp/src/app/features/private/private.component.ts
@@ -5,11 +5,10 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 
 @Component({
-  selector: 'app-private',
-  standalone: true,
-  imports: [RouterModule,MatToolbarModule, MatButtonModule, MatIconModule],
-  templateUrl: './private.component.html',
-  styleUrl: './private.component.scss'
+    selector: 'app-private',
+    imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
+    templateUrl: './private.component.html',
+    styleUrl: './private.component.scss'
 })
 export class PrivateComponent {
 
