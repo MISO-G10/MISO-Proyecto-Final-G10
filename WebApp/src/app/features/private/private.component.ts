29050defe5215cbf2e42c4d9a332f4276732fd04
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-03-29 21:41:16 -0500

    barra de navegacion lateral implementada

--- WebApp/src/app/features/private/private.component.ts
+++ WebApp/src/app/features/private/private.component.ts
@@ -1,15 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, ViewChild } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-private',
-    imports: [RouterModule, MatToolbarModule, MatButtonModule, MatIconModule],
+    imports: [RouterModule,
+        CommonModule,
+         MatToolbarModule,
+          MatButtonModule, 
+          MatIconModule,
+          MatSidenavModule,
+          MatListModule,
+        ],
     templateUrl: './private.component.html',
     styleUrl: './private.component.scss'
 })
 export class PrivateComponent {
-
+    @ViewChild('sidenav') sidenav!: MatSidenav;
+    private readonly router = inject(Router);
+    isClickExpanded = false;
+    isHoverExpanded = false;
+    expandedWidth = 200;
+    collapsedWidth = 60;
+  
+    toggleByClick() {
+      this.isClickExpanded = !this.isClickExpanded;
+      this.isHoverExpanded = false; // Reset hover state when clicking
+    }
+  
+    expandByHover() {
+      if (!this.isClickExpanded) {
+        this.isHoverExpanded = true;
+      }
+    }
+  
+    collapseByHover() {
+      if (!this.isClickExpanded) {
+        this.isHoverExpanded = false;
+      }
+    }
+  
+    getSidenavWidth(): number {
+      if (this.isClickExpanded || this.isHoverExpanded) return this.expandedWidth;
+      return this.collapsedWidth;
+    }
+    logOut(){
+        this.router.navigate(['/login']);
+    }
 }
