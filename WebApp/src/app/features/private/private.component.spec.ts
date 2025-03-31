5d2a60238543a27dc6f32f832b927cf26fa1fcc3
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-03-30 21:18:36 -0500

    Fix:webapp test

--- WebApp/src/app/features/private/private.component.spec.ts
+++ WebApp/src/app/features/private/private.component.spec.ts
@@ -1,16 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { PrivateComponent } from './private.component';
+import { ActivatedRoute,provideRouter } from '@angular/router';
 
 describe('PrivateComponent', () => {
   let component: PrivateComponent;
   let fixture: ComponentFixture<PrivateComponent>;
 
+  // Mock para ActivatedRoute
+  const mockActivatedRoute = {
+    snapshot: {
+      paramMap: new Map(),
+      queryParamMap: new Map(),
+      data: {}
+    }
+  };
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PrivateComponent]
-    })
-    .compileComponents();
+      imports: [
+        PrivateComponent, // Tu componente standalone
+        
+      ],
+      providers: [
+        { provide: ActivatedRoute, useValue: mockActivatedRoute },
+        provideRouter([])
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PrivateComponent);
     component = fixture.componentInstance;
