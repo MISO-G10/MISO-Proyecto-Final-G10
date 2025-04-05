9b1b18f560b3a27ede46bb03dea618453f410539
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-04-05 13:54:45 -0500

    WebApp:fix testing modules

--- WebApp/src/app/features/private/sellers/create-seller/create-seller.component.spec.ts
+++ WebApp/src/app/features/private/sellers/create-seller/create-seller.component.spec.ts
@@ -1,14 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateSellerComponent } from './create-seller.component';
+import { SnackbarService } from '../../../../shared/ui/snackbar.service';
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting } from '@angular/common/http/testing';
+import { FormBuilder } from '@angular/forms';
+import { SellerService } from '../../../../core/services/seller.services';
 
 describe('CreateSellerComponent', () => {
   let component: CreateSellerComponent;
   let fixture: ComponentFixture<CreateSellerComponent>;
-
+  let mockSnackbarService: jasmine.SpyObj<SnackbarService>;
+  
   beforeEach(async () => {
+    mockSnackbarService = jasmine.createSpyObj('SnackbarService', ['success', 'error']);
     await TestBed.configureTestingModule({
-      imports: [CreateSellerComponent]
+      imports: [CreateSellerComponent],
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+        FormBuilder,
+        { provide: SellerService, useValue: {} }, // Mock básico
+        { provide: SnackbarService, useValue: mockSnackbarService }
+      ]
     })
     .compileComponents();
 
