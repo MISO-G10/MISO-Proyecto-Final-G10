bd0f71df8e2adf01a11785489f86372a4d1cc0b7
Paula Bohorquez <p.bohorqueza@uniandes.edu.co>
2025-04-06 22:05:50 -0500

    Fix formato fechas

--- WebApp/src/app/core/services/productos.services.ts
+++ WebApp/src/app/core/services/productos.services.ts
@@ -3,7 +3,7 @@ import { Injectable,inject } from "@angular/core";
 import { environment } from '../../../environment/environment';
 import { Producto } from "../../features/private/productos/models/producto";
 interface ProductoResponse {
-    createdAt: string;
+  createdAt: string;
     sku:string;
 }
 @Injectable({ providedIn: 'root' })
@@ -11,15 +11,22 @@ export class ProductoService{
     private readonly http = inject(HttpClient);
     private readonly apiProductosUrl = environment.inventarioUrl+'/createproduct';
 
-    crearProducto(producto: Producto) {
-        // Crear una copia del objeto y transformar el nombre del campo
-        const { fabricanteId, ...rest } = producto;
-        const productoData = {
-            ...rest,
-            fabricante_id: fabricanteId
-        };
-        
-        console.log('Datos enviados al backend:', productoData);
-        return this.http.post<ProductoResponse>(`${this.apiProductosUrl}`, productoData);
-    }
+  crearProducto(producto: Producto) {
+    // Crear una copia del objeto y transformar el nombre del campo
+    const { fabricanteId, ...rest } = producto;
+    
+    // Formatear las fechas como strings ISO
+    const fechaVencimiento = producto.perecedero ? new Date(producto.fechaVencimiento).toISOString() : null;
+    const tiempoEntrega = new Date(producto.tiempoEntrega).toISOString();
+    
+    const productoData = {
+      ...rest,
+      fechaVencimiento: fechaVencimiento,
+      tiempoEntrega: tiempoEntrega,
+      fabricante_id: fabricanteId
+    };
+    
+    console.log('Datos enviados al backend:', productoData);
+    return this.http.post<ProductoResponse>(`${this.apiProductosUrl}`, productoData);
+  }
 }
