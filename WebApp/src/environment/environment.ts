b10a744a5aa5df01a98872c2a08e202ab6b40c42
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-04-10 16:33:46 -0500

    Update environment.ts

--- WebApp/src/environment/environment.ts
+++ WebApp/src/environment/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
     apiUrl:"http://localhost:3000",
-    inventarioUrl: "http://localhost:3003/inventarios"
+    inventarioUrl: "http://localhost:3003/inventarios",  
+    salesUrl: "http://localhost:3002",
+    fabricantesUrl:"http://localhost:3001"    
 }
