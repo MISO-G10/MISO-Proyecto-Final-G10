05597b1b557eb4bb2f177c4f2e75f95092f67ef0
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-04-10 16:37:20 -0500

    Update sales.service.ts

--- WebApp/src/app/features/private/sales/sales.service.ts
+++ WebApp/src/app/features/private/sales/sales.service.ts
@@ -3,14 +3,14 @@ import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { SalesPlan } from './models/sales';
 import { SnackbarService } from '../../../shared/ui/snackbar.service';
-
+import { environment } from '../../../environment/environment';
 @Injectable({
   providedIn: 'root'
 })
 export class SalesService {
   private readonly http = inject(HttpClient);
   private readonly snackbarService = inject(SnackbarService);
-  private readonly apiUrl = 'http://localhost:3002';
+  private readonly apiUrl = environment.salesUrl;
 
   constructor() {
   }
