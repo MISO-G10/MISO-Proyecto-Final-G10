d3c45af43fa611e8a691c132254b03cdd4e3d07f
Felipe Valencia <pipeedev@gmail.com>
2025-04-10 17:40:50 -0400

    fix:  env urls

--- WebApp/src/environment/environment.prod.ts
+++ WebApp/src/environment/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  apiUrl:"https://ccps1.wym.services/usuarios",
+  apiUrl:"https://ccps1.wym.services",
   inventarioUrl: "https://ccps1.wym.services/inventarios",
   fabricanteUrl: "https://ccps1.wym.services",
   salesUrl: "https://ccps1.wym.services/ventas"
