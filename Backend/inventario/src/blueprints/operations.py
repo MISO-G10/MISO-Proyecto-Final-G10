979a9aea036e3639b279740dbe204483b2880943
juanjice29 <jm.jimenezc12@uniandes.edu.co>
2025-04-27 20:58:11 -0500

    merge develop

--- Backend/inventario/src/blueprints/operations.py
+++ Backend/inventario/src/blueprints/operations.py
@@ -1,14 +1,14 @@
-from flask import jsonify, request, Blueprint, g
-from src.utils.validate_token import token_required
-from ..commands.assign_producto_bodega import AssignProductoBodega
-from ..commands.clean import Clean
-from ..commands.create_bodega import CreateBodega
+from flask import Flask, jsonify, request, Blueprint, g
 from ..commands.create_producto import Create
-from ..commands.get_producto_ubicacion import GetProductoUbicacion
 from ..commands.listar_productos import ListProductos
+from ..commands.clean import Clean
 from src.utils.validate_token import token_required
+from ..commands.create_bodega import CreateBodega
+from ..commands.get_producto_ubicacion import GetProductoUbicacion
+from ..commands.assign_producto_bodega import AssignProductoBodega
 
 import os
+
 
 operations_blueprint = Blueprint('inventarios', __name__)
 
@@ -26,7 +26,6 @@ def reset_fabricante_database():
 
     return jsonify(result), 200
 
-
 # Crear productos asociados a un fabricante
 @operations_blueprint.route("/createproduct", methods=['POST'])
 @token_required
@@ -34,25 +33,25 @@ def create_producto():
     json_data = request.get_json()
     if not json_data:
         return jsonify({"error": "El cuerpo de la solicitud no puede estar vacío"}), 400
-
+    
     if "fabricante_id" not in json_data:
         return jsonify({"error": "Requerido valor fabricante_id para crear un producto"}), 400
     current_usuario = g.current_usuario
     result = Create(current_usuario, json_data).execute()
-
+    
     # Verificar si el resultado es una tupla (respuesta, código)
     if isinstance(result, tuple) and len(result) == 2:
         return jsonify(result[0]), result[1]
-
+    
     # Si no es una tupla, es una respuesta exitosa
     return jsonify(result), 201
 
 # Listar todos los productos
 @operations_blueprint.route("/productos", methods=['GET'])
+@token_required
 def list_productos():
     result = ListProductos().execute()
     return jsonify(result), 200
-
 
 @operations_blueprint.route("/productos/ubicacion", methods=['GET'])
 @token_required
