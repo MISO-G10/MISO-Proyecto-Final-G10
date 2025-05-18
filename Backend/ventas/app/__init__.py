008fcc0d9aafb88731a90d3a897d2a6e89d0c65f
Andres Garcia <oa.garcia2@uniandes.edu.co>
2025-05-18 02:14:25 -0500

    mejoras en el micro de ventas

--- Backend/ventas/app/__init__.py
+++ Backend/ventas/app/__init__.py
@@ -139,5 +139,12 @@ def create_app():
         from app.models import sales_plan_seller, sales_plan
 
         db.create_all()
+    
+    # Registrar función para limpiar conexiones a la base de datos al final de cada petición
+    # @app.teardown_appcontext
+    # def shutdown_session(exception=None):
+    #     db.session.remove()
+    #     if os.environ.get('FLASK_ENV') == 'development':
+    #         print("Sesiones de base de datos limpiadas")
 
     return app
